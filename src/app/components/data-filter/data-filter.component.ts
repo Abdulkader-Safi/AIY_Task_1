f9fb1d10a319feec523364c2ab3d7c66104c3fe1
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-03-24 16:37:38 +0200

    Data Filter Section Done

--- src/app/components/data-filter/data-filter.component.ts
+++ src/app/components/data-filter/data-filter.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-data-filter',
   templateUrl: './data-filter.component.html',
-  styleUrls: ['./data-filter.component.scss']
+  styleUrls: ['./data-filter.component.scss'],
 })
-export class DataFilterComponent {
+export class DataFilterComponent implements OnInit {
+  _num1 = 10;
+  _num2 = 10;
+  _step = 1;
+  faArrowRight = faArrowRight;
+  faArrowLeft = faArrowLeft;
 
+  ngOnInit(): void {}
+
+  incrementNum1Value(step: number = 1): void {
+    const inputValue = this._num1 + step;
+
+    this._num1 = inputValue;
+  }
+
+  incrementNum2Value(step: number = 1): void {
+    const inputValue = this._num2 + step;
+
+    this._num2 = inputValue;
+  }
 }
