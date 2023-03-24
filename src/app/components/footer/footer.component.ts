ced58e31b04d73beac2ffd93c1b0b3b3d689f86e
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-03-24 19:12:02 +0200

    Footer Section Done

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  faSignOutAlt = faSignOutAlt;
 }
